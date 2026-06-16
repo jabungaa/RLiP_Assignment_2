f4b5566bb22b12131f71c4c96b8ee20f0661a29b
root <root@HTH-7950X.localdomain>
2026-06-16 15:19:43 +0200

    add run helper

---
+++ run_helper_Tianhao.sh
@@ -0,0 +1,32 @@
+#python train.py grid_configs/A1_grid.npy --no_gui --episodes 100000 --iter 200 --sigma 0
+#python train.py grid_configs/A1_grid.npy --no_gui --episodes 100000 --iter 200 --sigma 0
+#python train.py grid_configs/large_grid.npy --no_gui --episodes 100000 --iter 200 --sigma 0
+#python train.py grid_configs/large_grid.npy --no_gui --episodes 100000 --iter 200 --sigma 0
+#python train.py grid_configs/super_hard.npy --no_gui --episodes 2000 --iter 200 --sigma 0 --train_gamma 0.999
+
+
+python test_ppo.py --grid grid_configs/super_hard.npy --episodes 5000 --reward default --gamma 0.999 --step_penalty_threshold 50 --fourier_freqs 64
+
+
+#python test_dqn.py --grid grid_configs/small_grid.npy --episodes 2000 --reward low --gamma 0.99 --step_penalty_threshold 50
+#python run_experiments.py --grid grid_configs/super_hard.npy --episodes 2000 --iter 200 --sigma 0 --train_gamma 0.999
+
+# python test_ppo.py \
+#     --grid grid_configs/large_grid.npy \
+#     --episodes 3000 \
+#     --policy_lr 1e-3 \
+#     --value_lr 1e-3 \
+#     --entropy_coef 0.05 \
+#     --step_penalty_threshold 30 \
+#     --reward low \
+#     --replay_buffer_size 1000 \
+
+# python test_ppo.py \
+#     --grid grid_configs/small_grid.npy \
+#     --reward bfs \
+#     --activation tanh \
+#     --entropy_coef 0.2 \
+#     --policy_lr 1e-3 \
+#     --episodes 500
+
+
