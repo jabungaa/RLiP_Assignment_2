fb408fa9a29aec6cffc5944f063cfbfc0759ea8f
root <root@HTH-7950X.localdomain>
2026-06-19 01:59:48 +0200

    PPO worked in both small and medium restaurant

--- run_helper_Tianhao.sh
+++ run_helper_Tianhao.sh
@@ -4,10 +4,12 @@
 #python train.py grid_configs/large_grid.npy --no_gui --episodes 100000 --iter 200 --sigma 0
 #python train.py grid_configs/super_hard.npy --no_gui --episodes 2000 --iter 200 --sigma 0 --train_gamma 0.999
 
-#this one works well
+#this on works well
 #python test_ppo.py --grid grid_configs/small_grid.npy --episodes 500 --reward high --gamma 0.999 --fourier_freqs 64 --move_distance 0.2 --replay_capacity 16384 --activation relu
-#testing on larger grid
-python test_ppo.py --grid grid_configs/restaurant_small.npy --episodes 10000 --reward high --gamma 0.999 --fourier_freqs 16 --move_distance 0.5 --replay_capacity 16384 --save_train_images --activation relu --entropy_coef 0.005 --greedy_eval_interval 20
+#works on small restaurant
+#python test_ppo.py --grid grid_configs/restaurant_small.npy --episodes 10000 --reward high --gamma 0.999 --fourier_freqs 16 --move_distance 0.5 --replay_capacity 16384 --save_train_images --activation relu --entropy_coef 0.005 --greedy_eval_interval 20
+#test on middle restaurant
+python test_ppo.py --grid grid_configs/restaurant_medium.npy --episodes 10000 --reward high --gamma 0.999 --fourier_freqs 16 --move_distance 0.5 --replay_capacity 16384 --save_train_images --activation relu --entropy_coef 0.005 --greedy_eval_interval 20 --train_start_mode fixed
 
 #python test_dqn.py --grid grid_configs/small_grid.npy --episodes 2000 --reward low --gamma 0.99 --step_penalty_threshold 50
 #python run_experiments.py --grid grid_configs/super_hard.npy --episodes 2000 --iter 200 --sigma 0 --train_gamma 0.999
