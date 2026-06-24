98ea12eb8e8ab61e534070c6b7b2e45f61ee9507
Simeon Nikolov <98447748+jabungaa@users.noreply.github.com>
2026-06-24 23:09:25 +0200

    Delete run_helper_PPO.sh

--- run_helper_PPO.sh
+++
@@ -1,8 +0,0 @@
-
-
-
-#works on middle restaurant greedy eval interval 20
-#python test_ppo.py --grid grid_configs/restaurant_medium.npy --episodes 10000 --reward high --gamma 0.999 --fourier_freqs 16 --move_distance 0.5 --replay_capacity 16384 --save_train_images --activation relu --entropy_coef 0.005 --greedy_eval_interval 20 --train_start_mode fixed
-#testing stochastic start mode
-python test_ppo.py --grid grid_configs/restaurant_medium.npy --episodes 10000 --reward high --gamma 0.999 --fourier_freqs 16 --move_distance 0.5 --replay_capacity 16384 --save_train_images --activation relu --entropy_coef 0.005 --greedy_eval_interval 20 --train_start_mode fixed --sigma 0.05
-
